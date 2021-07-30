08f0264ed193e4dae4ce7d8f6ad4f7c51da4cbf3
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-30 13:57:40 +0900

    期限のカラムとview、controllerへの追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_041321) do
+ActiveRecord::Schema.define(version: 2021_07_30_045400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_07_30_041321) do
     t.boolean "finished", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expired_at"
   end
 
 end
